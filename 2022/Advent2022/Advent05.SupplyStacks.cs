811e3452955be252a7bc51d5e9290fdf4dd8c0dd
berven <jeremy.berven@7factor.io>
2022-12-06 00:33:33 -0500

    Converted problem 5 to use stacks

--- 2022/Advent2022/Advent05.SupplyStacks.cs
+++ 2022/Advent2022/Advent05.SupplyStacks.cs
@@ -1,31 +1,28 @@
-﻿using Advent2022.Models;
+﻿using System.Collections;
+using Advent2022.Models;
 using AdventShared;
 
 namespace Advent2022;
 
 public static class SupplyStacks
 {
-    public static Dictionary<int, List<char>> BuildSupplyStacks(List<string> stackStrings)
+    public static Dictionary<int, Stack<char>> BuildSupplyStacks(List<string> stackStrings)
     {
-        var stacks = new Dictionary<int, List<char>>();
+        var stacks = new Dictionary<int, Stack<char>>();
         
         var stackCount = (stackStrings.First().Length + 1) / 4;
+        stackStrings.Reverse();
         foreach (var stackString in stackStrings)
         {
             for (var i = 1; i <= stackCount; i++)
             {
                 if(!stacks.ContainsKey(i))
-                    stacks.Add(i, new List<char>());
+                    stacks.Add(i, new Stack<char>());
 
                 var value = stackString[(i - 1) * 4 + 1];
                 if(value != ' ')
-                    stacks[i].Add(stackString[(i-1) * 4 + 1]);
+                    stacks[i].Push(stackString[(i-1) * 4 + 1]);
             }
-        }
-
-        foreach (var stack in stacks)
-        {
-            stack.Value.Reverse();
         }
 
         return stacks;
@@ -36,20 +33,20 @@ public static class SupplyStacks
         return new CraneModel(instructionStrings);
     }
 
-    public static string ExecuteInstructions(Dictionary<int, List<char>> stacks, CraneModel crane)
+    public static string ExecuteInstructions(Dictionary<int, Stack<char>> stacks, CraneModel crane)
     {
         crane.ExecuteCrateMover9000(stacks);
         return GetTopItemsFromStacks(stacks);
     }
 
-    public static string ExecuteMultiMoveInstructions(Dictionary<int, List<char>> stacks, CraneModel crane)
+    public static string ExecuteMultiMoveInstructions(Dictionary<int, Stack<char>> stacks, CraneModel crane)
     {
         crane.ExecuteCrateMover9001(stacks);
         return GetTopItemsFromStacks(stacks);
     }
     
-    private static string GetTopItemsFromStacks(Dictionary<int, List<char>> stacks)
+    private static string GetTopItemsFromStacks(Dictionary<int, Stack<char>> stacks)
     {
-        return string.Join("", stacks.Select(x => x.Value.Last()));
+        return string.Join("", stacks.Select(x => x.Value.First()));
     }
 }
--- 2022/Advent2022/Models/CraneModel.cs
+++ 2022/Advent2022/Models/CraneModel.cs
@@ -9,26 +9,29 @@
             Instructions = instructions.Select(x => new CraneInstructionModel(x)).ToList();
         }
 
-        public void ExecuteCrateMover9000(Dictionary<int, List<char>> stacks)
+        public void ExecuteCrateMover9000(Dictionary<int, Stack<char>> stacks)
         {
             foreach (var instruction in Instructions)
             {
                 for (int i = 1; i <= instruction.CountToMove; i++)
                 {
-                    var itemToMove = stacks[instruction.FromStack].Last();
-                    stacks[instruction.ToStack].Add(itemToMove);
-                    stacks[instruction.FromStack].RemoveAt(stacks[instruction.FromStack].Count - 1);
+                    var itemToMove = stacks[instruction.FromStack].Pop();
+                    stacks[instruction.ToStack].Push(itemToMove);
                 }
             }
         }
 
-        public void ExecuteCrateMover9001(Dictionary<int, List<char>> stacks)
+        public void ExecuteCrateMover9001(Dictionary<int, Stack<char>> stacks)
         {
             foreach (var instruction in Instructions)
             {
-                var itemsToMove = stacks[instruction.FromStack].TakeLast(instruction.CountToMove);
-                stacks[instruction.ToStack].AddRange(itemsToMove);
-                stacks[instruction.FromStack].RemoveRange(stacks[instruction.FromStack].Count - instruction.CountToMove, instruction.CountToMove);
+                var itemsToMove = stacks[instruction.FromStack].Take(instruction.CountToMove);
+
+                foreach (var item in itemsToMove.Reverse())
+                {
+                    stacks[instruction.FromStack].Pop();
+                    stacks[instruction.ToStack].Push(item);
+                }
             }
         }
     }
