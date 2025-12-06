00d7cb793bcaadc74cc279cd7481b1da80c87ecc
Jeremy Berven <jeremy.berven@7factor.io>
2025-12-05 20:18:09 -0600

    Implement Advent 4

---
+++ 2025/Advent2025/Advent04/PaperRows.cs
@@ -0,0 +1,138 @@
+﻿namespace Advent2025.Advent04;
+
+public class PaperRows
+{
+    public List<char> Row { get; set; }
+
+    public PaperRows(string input)
+    {
+        Row = input.ToList();
+    }
+
+    public int CountFree(string previousRow, string nextRow)
+    {
+        var freeCount = 0;
+        for (int i = 0; i < Row.Count; i++)
+        {
+            if (Row[i] != '@')
+            {
+                continue;
+            }
+            
+            var adjacentPapers = 0;
+            if (!string.IsNullOrWhiteSpace(previousRow))
+            {
+                if (i != 0)
+                {
+                    adjacentPapers += previousRow[i - 1] == '@' ? 1 : 0;
+                }
+                adjacentPapers += previousRow[i] == '@' ? 1 : 0;
+                if (i != Row.Count - 1)
+                {
+                    adjacentPapers += previousRow[i + 1] == '@' ? 1 : 0;
+                }
+            }
+            
+            if (i != 0)
+            {
+                adjacentPapers += Row[i - 1] == '@' ? 1 : 0;
+            }
+            if (i != Row.Count - 1)
+            {
+                adjacentPapers += Row[i + 1] == '@' ? 1 : 0;
+            }
+            
+            if (!string.IsNullOrWhiteSpace(nextRow))
+            {
+                if (i != 0)
+                {
+                    adjacentPapers += nextRow[i - 1] == '@' ? 1 : 0;
+                }
+                adjacentPapers += nextRow[i] == '@' ? 1 : 0;
+                if (i != Row.Count - 1)
+                {
+                    adjacentPapers += nextRow[i + 1] == '@' ? 1 : 0;
+                }
+            }
+
+            if (adjacentPapers < 4)
+            {
+                freeCount++;
+            }
+        }
+
+        return freeCount;
+    }
+    
+    public int CountAndUpdateFree(PaperRows? previousRow, PaperRows? nextRow)
+    {
+        var freeCount = 0;
+        for (int i = 0; i < Row.Count; i++)
+        {
+            if (Row[i] != '@')
+            {
+                continue;
+            }
+            
+            var adjacentPapers = 0;
+            if (previousRow?.Row != null)
+            {
+                if (i != 0)
+                {
+                    adjacentPapers += IsCurrentlyOccupied(previousRow.Row[i - 1]) ? 1 : 0;
+                }
+                adjacentPapers += IsCurrentlyOccupied(previousRow.Row[i]) ? 1 : 0;
+                if (i != Row.Count - 1)
+                {
+                    adjacentPapers += IsCurrentlyOccupied(previousRow.Row[i + 1]) ? 1 : 0;
+                }
+            }
+            
+            if (i != 0)
+            {
+                adjacentPapers += IsCurrentlyOccupied(Row[i - 1]) ? 1 : 0;
+            }
+            if (i != Row.Count - 1)
+            {
+                adjacentPapers += IsCurrentlyOccupied(Row[i + 1]) ? 1 : 0;
+            }
+            
+            if (nextRow?.Row != null)
+            {
+                if (i != 0)
+                {
+                    adjacentPapers += IsCurrentlyOccupied(nextRow.Row[i - 1]) ? 1 : 0;
+                }
+                adjacentPapers += IsCurrentlyOccupied(nextRow.Row[i]) ? 1 : 0;
+                if (i != Row.Count - 1)
+                {
+                    adjacentPapers += IsCurrentlyOccupied(nextRow.Row[i + 1]) ? 1 : 0;
+                }
+            }
+
+            if (adjacentPapers < 4)
+            {
+                freeCount++;
+                Row[i] = 'x';
+            }
+        }
+
+        return freeCount;
+    }
+    
+    public void CleanupOccupied()
+    {
+        for (int i = 0; i < Row.Count; i++)
+        {
+            if (Row[i] == 'x')
+            {
+                Row[i] = '.';
+            }    
+        }
+    }
+
+    private bool IsCurrentlyOccupied(char value)
+    {
+        return value == '@' || value == 'x';
+    }
+}
---
+++ 2025/Advent2025/Advent04/Printing.cs
@@ -0,0 +1,51 @@
+﻿using Advent2025.Advent03;
+
+namespace Advent2025.Advent04;
+
+public class Printing
+{
+    public int TotalAvailableRolls(List<string> input)
+    {
+        var paperRows = input.Select(x => new PaperRows(x)).ToList();
+        
+        var totalCount = paperRows.First().CountFree("", input[1]);
+        for (int i = 1; i < input.Count - 1; i++)
+        {
+            totalCount += paperRows[i].CountFree(input[i - 1], input[i + 1]);
+        }
+        totalCount += paperRows.Last().CountFree(input[input.Count - 2], "");
+
+        return totalCount;
+    }
+    
+    public int OngoingAvailableRolls(List<string> input)
+    {
+        var paperRows = input.Select(x => new PaperRows(x)).ToList();
+        var totalFree = 0;
+        int lastFree;
+        do
+        {
+            lastFree = GetRemovedPaperCount(paperRows);
+            totalFree += lastFree;
+
+            foreach (var row in paperRows)
+            {
+                row.CleanupOccupied();
+            }
+        } while(lastFree > 0);
+
+        return totalFree;
+    }
+
+    private int GetRemovedPaperCount(List<PaperRows> paperRows)
+    {
+        var totalCount = paperRows.First().CountAndUpdateFree(null, paperRows[1]);
+        for (int i = 1; i < paperRows.Count - 1; i++)
+        {
+            totalCount += paperRows[i].CountAndUpdateFree(paperRows[i - 1], paperRows[i + 1]);
+        }
+        totalCount += paperRows.Last().CountAndUpdateFree(paperRows[paperRows.Count - 2], null);
+        
+        return totalCount;
+    }
+}
