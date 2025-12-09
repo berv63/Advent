3724d9855b8483d9a036a7f5610f4421db20a77f
Jeremy Berven <jeremy.berven@7factor.io>
2025-12-09 13:03:04 -0600

    Implement 9 part 1

---
+++ 2025/Advent2025/Advent09/MovieTheater.cs
@@ -0,0 +1,17 @@
+﻿namespace Advent2025.Advent09;
+
+public class MovieTheater
+{
+    public ulong LargestRectangleArea(List<string> input)
+    {
+        var id = 0;
+        var tiles = input.Select(row => new Tile(row, id++)).ToList();
+        
+        foreach (var tile in tiles)
+        {
+            tile.CalculateTileAreas(tiles);
+        }
+
+        return tiles.SelectMany(x => x.TileAreas.Select(y => y.Value)).ToList().Max();
+    }
+}
---
+++ 2025/Advent2025/Advent09/Tile.cs
@@ -0,0 +1,34 @@
+﻿namespace Advent2025.Advent09;
+
+public class Tile
+{
+    public int Id { get; set; }
+    public int XCoordinate { get; set; }
+    public int YCoordinate { get; set; }
+    
+    public Dictionary<int, ulong> TileAreas { get; set; } = new();
+
+    public Tile(string input, int id)
+    {
+        var stringParts = input.Split(',').ToList();
+        XCoordinate = int.Parse(stringParts[0]);
+        YCoordinate = int.Parse(stringParts[1]);
+        Id = id;
+    }
+    
+    public void CalculateTileAreas(List<Tile> tiles)
+    {
+        foreach (var tile in tiles)
+        {
+            if (tile.Id == Id || TileAreas.ContainsKey(tile.Id))
+            {
+                continue;
+            }
+            
+            var area = ((ulong)Math.Abs(tile.XCoordinate - XCoordinate) + 1) * ((ulong)Math.Abs(tile.YCoordinate - YCoordinate) + 1);
+            
+            TileAreas[tile.Id] = area;
+            tile.TileAreas[Id] = area;
+        }
+    }
+}
